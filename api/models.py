4eafc1afca4de9626f3980c0008e988346fa9630
souad <souadquzmar13@gmail.com>
2026-08-24 14:40:31 +0300

    update User model

--- api/models.py
+++ api/models.py
@@ -1,7 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
