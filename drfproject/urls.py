eb4a4928e28e54473e102ea167d17c88f844b49d
souad <souadquzmar13@gmail.com>
2026-08-25 17:31:16 +0300

    add user registration

--- drfproject/urls.py
+++ drfproject/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from api.views import RegisterView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', RegisterView.as_view(), name='register')
 ]
