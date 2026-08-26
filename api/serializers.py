96d4b277783ba2bc535723d8a84311cb13bc00c8
souad <souadquzmar13@gmail.com>
2026-08-26 10:12:51 +0300

    update announcement serializer

--- api/serializers.py
+++ api/serializers.py
@@ -2,10 +2,10 @@ from rest_framework import serializers
 from .models import Announcement
 
 class AnnouncementSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(read_only=True, slug_field='username')
     class Meta:
         model = Announcement
         fields = ['id', 'author', 'title', 'content', 'created_at']
-        extra_kwargs = {'author' : {'read_only' : True}}
 
     def create(self, validated_data):
         request = self.context.get('request')
