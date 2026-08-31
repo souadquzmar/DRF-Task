c8bb8f41c9ce0b38d2632cb247609f343f147be8
souad <souadquzmar13@gmail.com>
2026-08-31 16:00:01 +0300

    add .env.example

--- drfproject/settings.py
+++ drfproject/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ocffa)0gc3!+x7jz1hm25wygs!_e5*%u0f8f_z4m_a2n!+nb+r'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
 
 AUTH_USER_MODEL = 'api.User'
 # Application definition
